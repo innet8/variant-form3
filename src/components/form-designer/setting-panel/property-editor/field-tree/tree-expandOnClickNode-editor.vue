9c02c62c758621b9865df1adacc5193db7ed366f
Z M <403724027@qq.com>
2022-12-19 16:28:53 +0800

    VForm3树组件

---
+++ src/components/form-designer/setting-panel/property-editor/field-tree/tree-expandOnClickNode-editor.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <el-form-item :label="i18nt('designer.setting.expandOnClickNode')">
+    <el-switch v-model="optionModel.expandOnClickNode"></el-switch>
+  </el-form-item>
+</template>
+
+<script>
+  import i18n from "@/utils/i18n";
+
+  export default {
+    name: "tree-expandOnClickNode-editor",
+    mixins: [i18n],
+    props: {
+      designer: Object,
+      selectedWidget: Object,
+      optionModel: Object,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
